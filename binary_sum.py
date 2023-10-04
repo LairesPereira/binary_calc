81c6dbb4adaf00b39645a4f4dbeb5743582e3a13
Laires <lairespsoares@gmail.com>
2023-10-04 20:38:10 -0300

    calculadora estavel
    falta criar barreira caso o usuario nao digite binarios

---
+++ binary_sum.py
@@ -0,0 +1,16 @@
+from binary_to_decimal import translate_binary
+
+while True:
+    tool_choice = int(input('Digite 1 se deseja traduzir ou 2 se deseja somar: '))
+    if(tool_choice != 1 and tool_choice != 2):
+        print('Entrada inválida, seu puto!!!')
+    elif(tool_choice == 1):
+        print(translate_binary(input('Digite um número para traduzir: ' )))
+        break
+    else:
+        first_binary_translated = translate_binary(input('Digite o primeiro binario: '))
+        second_binary_translated = translate_binary(input('Digite o segundo binário: '))
+        print(f'A soma dos binários é: {int(first_binary_translated) + int(second_binary_translated)}')
+        break
+
+
---
+++ binary_to_decimal.py
@@ -0,0 +1,22 @@
+numbers_list = []
+
+def translate_binary(numbersList): 
+    # recebe uma string de numeros e joga em uma lista
+    for number in numbersList:
+        numbers_list.append(number)
+
+    # as potencias são iguais ao tamanho da lista -1 para que a ultima potencia seja zero
+    power = len(numbers_list) - 1
+    
+    final_sum = 0 
+    for number in numbers_list:
+        # calculamos a potencia somente se o valor for de um bit ligado
+        print(f'number: {number} power: {power}')
+        if(int(number) == 1):
+            final_sum += 2**power
+        power -= 1 #sempre diminuimos o valor da potencia para a proxima iteracao
+
+    print(f'soma: ', {final_sum})
+    numbers_list.clear()
+    return(final_sum)
+
